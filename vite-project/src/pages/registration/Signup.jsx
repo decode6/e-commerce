67de27dd9d36ebb904d126c52c13df5faf04c7b7
Decode <amitk.mca21@rvce.edu.in>
2023-12-15 22:38:16 +0530

    context api

--- vite-project/src/pages/registration/Signup.jsx
+++ vite-project/src/pages/registration/Signup.jsx
@@ -11,7 +11,7 @@ function Signup() {
     const [name, setName] = useState("");
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
-
+    // context menu
     const context = useContext(myContext);
     const { loading, setLoading } = context;
 

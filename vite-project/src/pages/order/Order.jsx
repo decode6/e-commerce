a84fd2c6740e626bb046bf9819157f87f7e15d34
Decode <amitkumar696945@gmail.com>
2024-01-12 22:42:03 +0530

    order section

--- vite-project/src/pages/order/Order.jsx
+++ vite-project/src/pages/order/Order.jsx
@@ -2,7 +2,7 @@ import React, { useContext } from 'react'
 import myContext from '../../context/data/myContext'
 import Layout from '../../components/layout/Layout'
 import Loader from '../../components/loader/Loader'
-
+//order section
 function Order() {
   const userid = JSON.parse(localStorage.getItem('user')).user.uid
   const context = useContext(myContext)
